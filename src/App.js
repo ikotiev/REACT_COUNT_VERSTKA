25cb26f0214e55edb2c5700023a2148be2d4c8f7
ikotiev <ikotiev@mail.ru>
2022-11-24 14:16:50 +0300

    count

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className='Main'>
+    <div className="Header">
+      <div className='Logo'>
+        <div className='Logo1'>C</div>
+        <div className='Logo2'>ounter</div>
+      </div>
+      <div className='Menu'>
+        <div className='bntMain'>Главная</div>
+        <div className='btnAboutUs'> О нас </div>
+        <div className='bntContacts'>Контакты</div>
+      </div>
     </div>
+    <div className='MainContent'>
+      <div className='Number'>140</div> 
+    </div>
+    <div className='Buttons'>
+       <div className='ButtonsSmall'>
+          <div className='btn1'>Увеличить</div>
+          <div className='btn2'>Уменьшить</div>
+          <div className='btn3'>Сбросить</div>
+        </div>
+      </div>
+    <div className='Footer'>
+      
+        <div className='FooterLogo'>
+          <div className='FLogo1'>C</div>
+          <div className='FLogo2'>ounter</div>
+        </div>
+        <div className='btn4'>Партнерам</div>
+        <div className='bnt5'>Разработчикам</div>
+        <div className='btn6'>Вакансии</div>
+        <div className='btn7'>OOO"Интукод" 2020г.</div>
+      
+    </div>
+
+  </div>
+  </>
   );
 }
 
